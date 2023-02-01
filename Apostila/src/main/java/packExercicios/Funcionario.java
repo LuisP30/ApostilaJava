231b30d18f67463b36f2010273795413884caf76
LuisP30 <pimenta.lh2@gmail.com>
2023-02-01 10:06:25 -0300

    Concluindo exercício da apostila java

--- Apostila/src/main/java/packExercicios/Funcionario.java
+++ Apostila/src/main/java/packExercicios/Funcionario.java
@@ -1,12 +1,38 @@
 package packExercicios;
 public class Funcionario {
-public String nome;
-public String departamento;
-public double salario;
-public String dataEntrada;
-String rg;
-boolean estaNaEmpresa;
+private String nome;
+private String departamento;
+private double salario;
+private String dataEntrada;
+private String rg;
+private boolean estaNaEmpresa;
+private int id = 0;
+public static int codigo = 1;
 
+//Métodos especiais
+public void setSalario (double salario){
+    this.salario = salario;
+}
+public double getSalario (){
+    return salario;
+}
+public String getNome(){
+    return nome;
+}
+public int getId(){
+    return id;
+}
+//Construtor padrão:
+public Funcionario(){
+    this.id = Funcionario.codigo++;
+}
+//Construtor parametrizado:
+public Funcionario(String nome){
+    this.nome=nome;
+    this.id = Funcionario.codigo++;
+}
+
+//Métodos comuns
 public void bonifica(double aumento){
 this.salario+=aumento;
 }
--- Apostila/src/main/java/packExercicios/MainFuncionario.java
+++ Apostila/src/main/java/packExercicios/MainFuncionario.java
@@ -2,11 +2,30 @@
 package packExercicios;
 public class MainFuncionario {
     public static void main(String[]args){
-    Funcionario f1 = new Funcionario();
-    f1.nome = "Hugo";
-    f1.salario = 100;
+    Funcionario f1 = new Funcionario("Hugo");
+    f1.setSalario(100);
     f1.bonifica(50);
-    System.out.println("Salário atual: "+f1.salario);
-    System.out.println("Ganho anual:"+f1.calculaGanhoAnual());
+    Funcionario f2 = new Funcionario("Jonas");
+    f2.setSalario(250);
+    f2.bonifica(30);
+    Funcionario f3 = new Funcionario("Mateus");
+    f3.setSalario(60);
+    f3.bonifica(60);
+    
+    
+    //Informações de Funcionário f1:
+    System.out.println("Nome: "+f1.getNome()+"\nSalário atual: "+f1.getSalario()+"\nGanho anual: "+f1.calculaGanhoAnual()+"\nID: "+f1.getId());
+    //Informações de Funcionário f2:
+    System.out.println("\nNome: "+f2.getNome()+"\nSalário atual: "+f2.getSalario()+"\nGanho anual: "+f2.calculaGanhoAnual()+"\nID: "+f2.getId());
+    //Informações de Funcionário f3:
+    System.out.println("\nNome: "+f3.getNome()+"\nSalário atual: "+f3.getSalario()+"\nGanho anual: "+f3.calculaGanhoAnual()+"\nID: "+f3.getId());
     }
+    
+    /*É necessária a utilização do construtor padrão pois sem ele ficaria apenas o construtor
+    parametrizado tornando-se assim obrigatório declarar os argumentos passados para 
+    ele na criação da classe Funcionário*/
+    
+    /*Sempre que criado um novo objeto, ele recebe um id único, sendo necessário apenas
+    o método Getter para retornar o valor do id do objeto. A utilização de um Setter não
+    se faz necessária visto que o valor do id já está sendo incrementado de forma automatica.*/
 }
